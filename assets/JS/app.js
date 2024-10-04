da9119bd1cb9cda9a670bb40d43bb8a26af1dfde
Your Name <you@example.com>
2024-10-04 16:49:57 -0700

    add font family

---
+++ assets/JS/app.js
@@ -0,0 +1,11 @@
+ function toggleAccordion(index) {
+            const content = document.getElementById('content' + index);
+    const plusIcon = content.previousElementSibling.querySelector('.accordion-icon-plus');
+    const minusIcon = content.previousElementSibling.querySelector('.accordion-icon-minus');
+    // Toggle content visibility
+    content.classList.toggle('hidden');
+
+    // Toggle plus and minus icons
+    plusIcon.classList.toggle('hidden');
+    minusIcon.classList.toggle('hidden');
+        }
